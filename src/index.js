ae4f03917b75cafbd77f40c5b85e45d52629fa82
Bryan CH <bryannsi@outlook.com>
2022-06-01 17:42:39 -0600

    helmet

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@
 import express from "express";
 import cors from "cors";
 import * as http from "http"; //ES 6
+import helmet from "helmet";
 // import authRoutes from "./src/routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet);
 // app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
